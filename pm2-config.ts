2e6847b555eecfcf61d53f191e2686f61d1555a6
Musicode1 <MusiCode3@gmail.com>
2025-06-22 00:33:45 +0300

    הוספת קונפיגורציה של pm2

---
+++ pm2-config.ts
@@ -0,0 +1,52 @@
+import type { PM2Config } from 'pm2-config';
+
+const config: PM2Config = {
+    apps: [{
+        name: "backendServer",
+        script: "./packages/server/src/index.ts",
+        interpreter: "bun",
+        env: {
+            NODE_ENV: "development",
+        },
+    }, {
+        name: 'rClone',
+        script: 'C:\\programs\\rclone',
+        args: [
+            'serve',
+            'dlna',
+            'GoogleDrive:"תיקיית-סרטונים-למסך"',
+            '--read-only',
+            '--vfs-cache-mode',
+            'full',
+
+            '--vfs-cache-max-age',
+            '999h0m0s',
+
+            '--cache-dir',
+            'C:\\Cache',
+
+            '--config',
+            'rclone.conf',
+
+            '--fast-list',
+            '--name',
+            '"סרטונים מגוגל דרייב"',
+            '--no-console',
+            '--rc',
+            '--rc-no-auth',
+            '--rc-web-gui',
+
+            '--rc-addr',
+            ':5572',
+
+            '--announce-interval',
+            '10s'
+        ]
+    }, {
+        name: "remoteControlServer",
+        script: './packages/proxy-server/src/index.ts',
+        interpreter: "bun"
+    }]
+}
+
+export default config;
