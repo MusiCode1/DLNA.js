bb2cd596df64c530f7805895d1021f3c721b8756
Musicode1 <MusiCode3@gmail.com>
2025-06-22 03:10:13 +0300

    הוספת interpreter_args להגדרות pm2

--- pm2.config.js
+++ pm2.config.js
@@ -9,34 +9,43 @@ const config = {
         env: {
             NODE_ENV: "development",
         },
-        cwd: './packages/server'
+        cwd: './packages/server',
+        interpreter_args: [
+            '--cwd=./packages/server',
+        ]
     }, {
         name: 'rClone',
         script: 'C:/programs/rclone/rclone.exe',
+        cwd: 'C:/programs/rclone/',
         args: [
             'serve',
             'dlna',
             'GoogleDrive:/תיקיית-סרטונים-למסך',
             '--read-only',
-            '--vfs-cache-mode','full',
-            '--config','C:/programs/rclone/rclone.conf',
-            '--vfs-cache-max-age','999h0m0s',
-            '--cache-dir','C:/Cache',
+            '--vfs-cache-mode', 'full',
+            '--config', 'C:/programs/rclone/rclone.conf',
+            '--vfs-cache-max-age', '999h0m0s',
+            '--cache-dir', 'C:/Cache',
             '--fast-list',
-            '--name','"סרטונים מגוגל דרייב"',
-            '--rc-addr',':5572',
-            '--announce-interval','10s',
+            '--name', '"סרטונים מגוגל דרייב"',
+            '--rc-addr', ':5572',
+            '--announce-interval', '10s',
             /* '--log-level', 'DEBUG', */
             /* '--no-console', */
             '--rc',
             '--rc-no-auth',
             '--rc-web-gui',
+
         ]
     }, {
         name: "remoteControlServer",
         script: './packages/proxy-server/src/index.ts',
         interpreter: "bun",
-        cwd: './packages/proxy-server'
+        cwd: './packages/proxy-server',
+        interpreter_args: [
+            '--cwd=./packages/proxy-server',
+        ]
+
     }]
 };
 
